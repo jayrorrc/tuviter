76fbc720fb28cac28a7885c7ee042a3351f7a955
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2016-01-30 15:48:26 -0300

    Add nav-bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/show'
-
     devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
     resources :posts
-    root 'posts#index'
+    root 'home#show'
 end

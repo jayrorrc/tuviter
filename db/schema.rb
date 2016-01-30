2963da55e6139c40a72ee350ae9c72d2ae25c1e9
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2016-01-30 11:45:18 -0300

    Add Outh collumns to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130135357) do
+ActiveRecord::Schema.define(version: 20160130141543) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20160130135357) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

1924f8aa596845404fbb27d9c3d210b69e2feb07
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2016-01-31 07:12:19 -0300

    Remove useless columns from table friendship

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131005244) do
+ActiveRecord::Schema.define(version: 20160131101002) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
-    t.string   "create"
-    t.string   "destroy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

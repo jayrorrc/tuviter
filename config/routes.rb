1394bf7b5d436a19ea1a3858f809d65790b17f57
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2016-01-30 21:28:44 -0300

    Create fuctions New Post and List My Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
     devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+
     resources :posts
+    get '/posts/new', to: 'patients#new', as: 'post_new'
+    get '/posts', to: 'patients#index', as: 'post_all'
+
     root 'home#show'
 end
